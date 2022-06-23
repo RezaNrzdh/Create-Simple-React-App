babbc484e3e256ae02d4dcbb8cb5b0d9a316843e
RezaNrzdh <reza.nrzdh@gmail.com>
2022-06-23 10:39:33 +0430

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,26 @@
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
 
 const Header = () => {
     return(
-        <header>
+        <HeaderStyled>
             <nav>
                 <h1>THE SIMPLE REACT APP</h1>
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
             </nav>
-        </header>
+        </HeaderStyled>
     );
 }
 
 export default Header;
+
+const HeaderStyled = styled.header`
+    display: flex;
+    width: 100%;
+    height: 80px;
+    background-color: white;
+    padding: 0 24px;
+    box-shadow: 0px 16px 48px rgba(0,0,0,0.08);
+    box-sizing: border-box;
+`;
